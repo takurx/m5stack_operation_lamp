709527477d8fc793a1ece98e542fd62ebb64e2b9
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2022-06-21 08:33:56 +0900

    add wifi_pass.h for ci pass
    add wifi_pass.h for ci pass

---
+++ include/wifi_pass.h
@@ -0,0 +1,30 @@
+#define BATH_BOT_VERSION "1.1.4"
+// test for Geekhouse Tsukuba
+#define WIFI_SSID "GeekHouseTsukuba-g"
+#define WIFI_PASS "tomorinao"
+
+const char* ca = \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n" \
+"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
+"d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
+"......n" \
+"......n" \
+"......n" \
+"......n" \
+"......n" \
+"......n" \
+"......n" \
+"......n" \
+"......n" \
+"......n" \
+"......n" \
+"......n" \
+"06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n" \
+"PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n" \
+"YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n" \
+"CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n" \
+"-----END CERTIFICATE-----\n";
+
+const char* hostname = "hooks.slack.com";
+const char* target_page = "/services/T0....../B0....../jB......";
